eccecc35af786c9c9a267ac39c2dc7e754c67310
peaceWorld <94433423+peaceWold@users.noreply.github.com>
2022-01-30 04:17:26 +0900

    update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import { render } from 'react-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Footer } from '@pmndrs/branding'
+// import { Footer } from '@pmndrs/branding'
 import useStore from './store'
 import './index.css';
+
+function Overlay(){
+  const loaded = useStore((state) => state.loaded)
+  const clicked = useStore((state) => state.clicked)
+  const api = useStore((state) => state.api)
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Overlay/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
